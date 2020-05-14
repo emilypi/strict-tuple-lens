b09da6390175e9312760b791b82a3ebe8acebab7
Emily Pillmore <emilypi@cohomolo.gy>
2020-05-14 10:57:40 -0400

    update cabal version, tighten bounds

---
+++ src/Data/Tuple/Strict/Lens/Combinators.hs
@@ -0,0 +1,22 @@
+-- |
+-- Module       : Data.Tuple.Strict.Lens
+-- Copyright    : (c) 2020 Emily Pillmore
+-- License      : BSD-style
+--
+-- Maintainer   : Emily Pillmore <emilypi@cohomolo.gy>
+-- Stability    : Experimental
+-- Portability  : portable
+--
+-- This module exports all instances defined for strict tuples.
+--
+-- Instances include:
+--
+--  - 'Control.Lens.Each'
+--  - 'Control.Lens.Empty'
+--  - 'Control.Lens.Field'
+--  - 'Control.Lens.Iso'
+--
+module Data.Tuple.Strict.Lens.Combinators
+( --* Each
+, each
+, )where
